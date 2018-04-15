8c6d6495325559fd153f194e2d4a95220bce89ea
ndohvich <ndohmoise@gmail.com>
2018-04-15 03:39:05 +0200

    first commit

---
+++ assets1/js/renderer.js
@@ -0,0 +1,10 @@
+var connexion = document.getElementById('con');
+
+connexion.addEventListener('click', function(){
+
+    var loginField = document.getElementById('login').value;
+    var mdpField = document.getElementById('mdp').value;
+    document.location.href = 'about.html';
+    
+});
+
